ef43fb4e648e8384346b2235e673590106ff5eda
singhgauravv <gauravs49804@gmail.com>
2024-06-29 22:41:55 +0530

    Configure node & flask server.

--- qa/node_server/app.js
+++ qa/node_server/app.js
@@ -1,11 +1,71 @@
 const express = require("express");
+const axios = require("axios");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 3050;
 
-app.get("/", (req, res) => {
-  res.send("NodeJS Backend Server");
+app.use(bodyParser.json());
+
+// KNN
+app.post("/api/knn", async (req, res) => {
+  const { attributes } = req.body;
+  const featuresArray = Object.values(attributes);
+
+  try {
+    response = await axios.post("http://localhost:5000/knn", featuresArray);
+    res.status(200).json(response.data);
+  } catch (err) {
+    res.status(500).send("Some Error Occured");
+  }
 });
 
+// gs-logistic
+app.post("/api/logictic/gs", async (req, res) => {
+  const { attributes } = req.body;
+  const featuresArray = Object.values(attributes);
+
+  try {
+    response = await axios.post(
+      "http://localhost:5000/logistic/gs",
+      featuresArray
+    );
+    res.status(200).json(response.data);
+  } catch (err) {
+    res.status(500).send("Some Error Occured");
+  }
+});
+
+// rs-logistic
+app.post("/api/logistic/rs", async (req, res) => {
+  const { attributes } = req.body;
+  const featuresArray = Object.values(attributes);
+
+  try {
+    response = await axios.post(
+      "http://localhost:5000/logistic/rs",
+      featuresArray
+    );
+    res.status(200).json(response.data);
+  } catch (err) {
+    res.status(500).send("Some Error Occured");
+  }
+});
+
+// rs-randomforest
+app.post("/api/randomforest/rs", async (req, res) => {
+  const { attributes } = req.body;
+  const featuresArray = Object.values(attributes);
+
+  try {
+    response = await axios.post(
+      "http://localhost:5000/randomforest/rs",
+      featuresArray
+    );
+    res.status(200).json(response.data);
+  } catch (err) {
+    res.status(500).send("Some Error Occured");
+  }
+});
 app.use("*", (req, res) => {
   res.send("Page doesn't exist");
 });
--- qa/python_server/app.py
+++ qa/python_server/app.py
@@ -10,44 +10,104 @@ rs_random_forest_model = pickle.load(open("../models/rs_rf_model.pkl", "rb"))
 app = Flask(__name__)
 
 
-def getNumericAttrs(data):
-    attrs = list(data["attributes"].keys())
-    numeric_attrs = [float(data["attributes"][attr]) for attr in attrs]
-    return numeric_attrs
-
-
 # Path to handle knn prediction.
 @app.route("/knn", methods=["POST"])
-def predictknn():
-    # Todo: Handle Data Formatting at the nodejs server
+def predict_knn():
     data = request.get_json(force=True)
-    prediction = knn_model.predict([getNumericAttrs(data)])
-    return str(prediction[0])
+    try:
+        # Ensure the data is a list of appropriate length
+        required_num_of_features = 13
+        if len(data) != required_num_of_features:
+            return (
+                jsonify(
+                    {
+                        "error": f"Expected {required_num_of_features} features, got {len(data)}"
+                    }
+                ),
+                400,
+            )
+
+        prediction = knn_model.predict([data])
+        print(prediction[0])
+        return str(prediction[0])
+
+    except Exception as e:
+        return jsonify({"error": "Error processing data", "details": str(e)}), 400
 
 
 # Path to handle gridsearch logistic regression prediction.
 @app.route("/logistic/gs", methods=["POST"])
 def predict_log_gs():
-    # Todo: Handle Data Formatting at the nodejs server
     data = request.get_json(force=True)
-    prediction = gs_log_regression_model.predict([getNumericAttrs(data=data)])
-    return str(prediction[0])
+    try:
+        # Ensure the data is a list of appropriate length
+        required_num_of_features = 13
+        if len(data) != required_num_of_features:
+            return (
+                jsonify(
+                    {
+                        "error": f"Expected {required_num_of_features} features, got {len(data)}"
+                    }
+                ),
+                400,
+            )
+
+        prediction = gs_log_regression_model.predict([data])
+        print(prediction[0])
+        return str(prediction[0])
+
+    except Exception as e:
+        return jsonify({"error": "Error processing data", "details": str(e)}), 400
 
 
 # Path to handle random forest logistic regression prediction.
-@app.route("/logistic/rf", methods=["POST"])
+@app.route("/logistic/rs", methods=["POST"])
 def predict_log_rs():
     data = request.get_json(force=True)
-    prediction = rs_log_regression_model.predict([getNumericAttrs(data=data)])
-    return str(prediction[0])
+    try:
+        # Ensure the data is a list of appropriate length
+        required_num_of_features = 13
+        if len(data) != required_num_of_features:
+            return (
+                jsonify(
+                    {
+                        "error": f"Expected {required_num_of_features} features, got {len(data)}"
+                    }
+                ),
+                400,
+            )
+
+        prediction = rs_log_regression_model.predict([data])
+        print(prediction[0])
+        return str(prediction[0])
+
+    except Exception as e:
+        return jsonify({"error": "Error processing data", "details": str(e)}), 400
 
 
 # Path to handle random forest randomized search prediction.
-@app.route("/randsearch/rf", methods=["POST"])
+@app.route("/randomforest/rs", methods=["POST"])
 def predict_ran_search():
     data = request.get_json(force=True)
-    prediction = rs_random_forest_model.predict([getNumericAttrs(data=data)])
-    return str(prediction[0])
+    try:
+        # Ensure the data is a list of appropriate length
+        required_num_of_features = 13
+        if len(data) != required_num_of_features:
+            return (
+                jsonify(
+                    {
+                        "error": f"Expected {required_num_of_features} features, got {len(data)}"
+                    }
+                ),
+                400,
+            )
+
+        prediction = rs_random_forest_model.predict([data])
+        print(prediction[0])
+        return str(prediction[0])
+
+    except Exception as e:
+        return jsonify({"error": "Error processing data", "details": str(e)}), 400
 
 
 if __name__ == "__main__":
